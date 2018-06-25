b3b79b1881f9b8caaa26e11a2c6b9a95563bffae
Student <sergey.bombin@intspirit.com>
2018-06-25 13:10:03 +0300

    catch send message

--- server.js
+++ server.js
@@ -16,14 +16,22 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+// connect
 io.sockets.on("connection", function(socket) {
   connections.push(socket);
   console.log(`Socket ${socket.id} is connected`);
   console.log(`Connected: ${connections.length} sockets connected`);
 
+  // disconnect
   socket.on("disconnect", function(data) {
     console.log(`Socket ${socket.id} is dissconnected`);
     connections.splice(connections.indexOf(socket));
     console.log(`Disconnected: ${connections.length} sockets connected`);
+  });
+
+  // send message
+  socket.on("send message", function(data) {
+    console.log(data);
+    io.sockets.emit("new message", { msg: data });
   });
 });
